13899cce48577537f3692788195dc0f79d7ba023
tsq <1530234656@qq.com>
2015-11-09 09:59:27 +0800

    add lazyload

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var app = require("express")();
-var data = require("./db");
-var config = {
-  port: process.env.NODE_PORT || 3000
-};
+var compression = require('compression');
+app.use(compression());
 
+var data = require("./db");
+var port = process.env.NODE_PORT || 3000
 
 app.set('views', './');
 app.set('view engine', 'ejs');
 app.all('*', function (req, res) {
   return res.render('index', {data: data});
 });
-app.listen(config.port, function () {
-  console.log('app listening on ' + config.port);
+app.listen(port, function () {
+  console.log('app listening on ' + port);
 });
