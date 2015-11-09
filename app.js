434d05a9b24fc275ef3610c8da93129bbbaa2cdd
tsq <1530234656@qq.com>
2015-11-09 13:26:59 +0800

    add detail panel

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ var compression = require('compression');
 app.use(compression());
 
 var data = require("./db");
-var port = process.env.NODE_PORT || 3000
+var port = process.env.NODE_PORT || 4000
 
 app.set('views', './');
 app.set('view engine', 'ejs');
 app.all('*', function (req, res) {
-  return res.render('index', {data: data});
+  return res.render('index', {data: data, map: JSON.stringify(getMap(data))});
 });
 app.listen(port, function () {
   console.log('app listening on ' + port);
 });
+
+function getMap(data) {
+  var map = {};
+  data.forEach(function (item) {
+    map[item.index] = item;
+  });
+  return map;
+}
