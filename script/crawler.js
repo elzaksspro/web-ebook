2201d6b8999969cd58c66fc198c14d6c7b948c0e
tsq <1530234656@qq.com>
2015-11-10 16:40:27 +0800

    add create time

--- script/crawler.js
+++ script/crawler.js
@@ -3,6 +3,12 @@ var fs = require("fs");
 var cheerio = require("cheerio");
 var request = require("request");
 var async = require("async");
+var data = require("./data.json");
+
+var existIndex = data.map(function (item) {
+  return item.index;
+});
+
 
 var config = {
   bookPagePrefix: 'http://it-ebooks.info/book/',
@@ -10,55 +16,39 @@ var config = {
   saveName: 'data.json'
 };
 
-db.sort(function (a, b) {
-  if (a.index > b.index) {
-    return 1;
-  }
-  if (a.index < b.index) {
-    return -1;
-  }
-  return 0;
-});
-
-var result = [];
-
-
 async.mapLimit(db, 5, function (item, cb) {
   getBookInfo(item, cb);
 }, function () {
-  result.sort(function (a, b) {
-    if (a.bookName > b.bookName) {
-      return 1;
-    }
-    if (a.bookName < b.bookName) {
-      return -1;
-    }
-    return 0;
-  });
 
-  fs.writeFileSync(config.saveName, JSON.stringify(result, null, 2));
+  fs.writeFileSync(config.saveName, JSON.stringify(data, null, 2));
   console.log('Congratulation!!! data has been saved into data.json');
 });
 
 function getBookInfo(item, cb) {
-  var url = config.bookPagePrefix + item.index;
-      console.log('crawling:', url);
-  request(url, function (err, res, body) {
-    if (err) {console.error(err);process.exit(1);}
-    var $ = cheerio.load(body);
-    var obj = {
-      bookName: $('[itemprop="name"]').text(),
-      postSrc: config.imgSrcPrefix + $('.ebook_view img').attr('src'),
-      datePublished :$('[itemprop="datePublished"]').text(),
-      numberOfPages :$('[itemprop="numberOfPages"]').text(),
-      originDlLink: $('[href^="http://filepi"]').attr('href'),
-      description: $('[itemprop="description"]').text(),
-      originPageLink: url,
-      index: item.index,
-      customBookName: item.bookName
-    };
-    console.log('info: [' + obj.bookName + '] is ok');
-    result.push(obj);
-    cb(null, obj);
-  });
+  if (existIndex.indexOf(item.index) !== -1) {
+    console.log('**********************');
+    cb(null);
+  } else {
+    var url = config.bookPagePrefix + item.index;
+    console.log('crawling:', url);
+    request(url, function (err, res, body) {
+      if (err) {console.error(err);process.exit(1);}
+      var $ = cheerio.load(body);
+      var obj = {
+        bookName: $('[itemprop="name"]').text(),
+        postSrc: config.imgSrcPrefix + $('.ebook_view img').attr('src'),
+        datePublished :$('[itemprop="datePublished"]').text(),
+        numberOfPages :$('[itemprop="numberOfPages"]').text(),
+        originDlLink: $('[href^="http://filepi"]').attr('href'),
+        description: $('[itemprop="description"]').text(),
+        originPageLink: url,
+        index: item.index,
+        customBookName: item.bookName,
+        createTime: Date.now()
+      };
+      console.log('info: [' + obj.bookName + '] is ok');
+      data.push(obj);
+      cb(null, obj);
+    });
+  }
 }
